1c7e236389ec92670bb67012e5d2a26806526259
Lin Chi Yu <chiyupc+linchiyu@users.noreply.github.com>
2021-09-13 22:18:44 -0300

    testing abp

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace learning_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World3!");
+            Pessoa x = new Pessoa();
+            x.Func();
+        }
+    }
+}
---
+++ pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+/*
+class Hello
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World");
+    }
+}
+*/
+
+class Pessoa
+{
+    public void Func()
+    {
+        Console.WriteLine("function pessoa");
+    }
+}
